a7e22c3127ac470acfb86f2a749a2e8de3ea409a
David Towson <david115632042@gmail.com>
2015-06-10 18:27:40 -0400

    Finished assignment

--- main.py
+++ main.py
@@ -12,8 +12,8 @@
 from string import punctuation
 from string import ascii_lowercase
 
-def char_counter():
-    input_file = open("input_file.txt", "r")
+def char_counter(file_name):
+    input_file = open(file_name, "r")
 
     punct = set(punctuation)
     punct.add("\n")
@@ -34,14 +34,14 @@ def char_counter():
                 letter_list[letter] = 0
 
     for key in letter_list.keys():
-        print("This file contains %s instances of %s" %(letter_list[key], key))
+        print("This file contains %s instances of %s" % (letter_list[key], key))
 
     input_file.close()
     return letter_list
 
 
-def word_counter():
-    input_file = open("input_file.txt", "r")
+def word_counter(file_name):
+    input_file = open(file_name, "r")
 
     word_list = {}
     for word in input_file.read().split():
@@ -55,53 +55,81 @@ def word_counter():
             word_list[word_lower] += 1
 
     for key in word_list.keys():
-        print("This file contains %s instances of %s" %(word_list[key], key))
+        print("This file contains %s instances of %s" % (word_list[key], key))
 
     input_file.close()
     return word_list
 
-def get_char_percents(input_data):
-    num_of_chars = get_total_chars(input_data)
+
+def get_char_percents(_char_nums, num_of_chars):
     char_percent_list = {}
     for letter in ascii_lowercase:
-        if letter in char_analysis:
-            char_percent = str((float(char_analysis[letter])/num_of_chars)*100)
+        if letter in _char_nums:
+            char_percent = str((float(_char_nums[letter])/num_of_chars)*100)
             char_percent_list[letter] = char_percent
-            print("%s consists of %s percent of input chars" %(str(letter), char_percent))
+            print("%s consists of %s percent of input chars" % (str(letter), char_percent))
         else:
-            print("%s consists of 0 percent of input chars" %(str(letter)))
-            char_analysis[letter] = 0
+            print("%s consists of 0 percent of input chars" % (str(letter)))
+            char_percent_list[letter] = 0
+
+    return char_percent_list
 
 
-def get_total_chars(input_data):
+def get_total_chars(_char_nums):
     char_totals = 0
-    for items in char_analysis:
-        char_totals += char_analysis[items]
+    for items in _char_nums:
+        char_totals += _char_nums[items]
     return char_totals
 
 
-def get_total_words(input_data):
+def get_total_words(_word_nums):
     word_total = 0
-    for items in word_analysis:
-        word_total += word_analysis[items]
+    for items in _word_nums:
+        word_total += _word_nums[items]
     return word_total
 
 
 if __name__ == '__main__':
-    # get file input and output name
     input_file_name = raw_input("Please enter the name of your input .txt file (without extension)\n")
     output_file_name = raw_input("Please enter the name of your output .txt file (without extension)\n")
-
     extended_input_file_name = input_file_name + ".txt"
     extended_output_file_name = output_file_name + ".txt"
 
-    char_analysis = char_counter()
-    word_analysis = word_counter()
+    char_nums = char_counter(extended_input_file_name)
+    word_nums = word_counter(extended_input_file_name)
 
+    total_chars = get_total_chars(char_nums)
+    total_words = get_total_words(word_nums)
     # time to process the char and word data
 
-    char_percents = get_char_percents(char_analysis)
+    char_percents = get_char_percents(char_nums, total_chars)
 
-    output_file = open(extended_output_file_name, "w")
+    output_file = open(extended_output_file_name, "r+")
+    output_file.write("Occurrences of each letter:\n")
 
-    num_of_words = get_total_words(word_analysis)
+    for letter in ascii_lowercase:
+        output_file.write("'%s' occurs %s times in the file.  %s percent of total\n" %
+                            (letter, char_nums[letter], char_percents[letter]))
+    output_file.write("\nTotal number of chars is %s\n" % (total_chars))
+    output_file.write("\nWord stats:\n")
+
+    for word in word_nums:
+        output_file.write("'%s' occurs %s times\n" % (word, word_nums[word]))
+    output_file.write("\nTotal number of words is %s\n" % (total_words))
+
+    sorted_dict = sorted(word_nums.values(), reverse=True)
+    top_ten_values = sorted_dict[0:10]
+    unique_top_ten_values = list(set(top_ten_values))
+    sorted_unique_top_ten_values = sorted(unique_top_ten_values, reverse=True)
+
+    top_word_list = []
+    for nums in sorted_unique_top_ten_values:
+        for key, value in word_nums.iteritems():
+            if value == nums:
+                top_word_list.append(key)
+        if len(top_word_list) >= 10:
+            break
+
+    for ranking in range(1, 11):
+        output_file.write("word ranked %s is : %s\n" % (str(ranking), top_word_list[ranking - 1]))
+    output_file.close()
